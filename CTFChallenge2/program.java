8069fe99c7bf426d9dc374988ecdf5611546ed34
Joseph Zhong <jzhong653@halelabs-iMac-2.local>
2018-06-14 13:21:29 -0500

    New Challenge

---
+++ CTFChallenge2/program.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class program
+{
+public static void main (String [] args)
+{
+  Scanner scan = new Scanner(System.in);
+  String flag = "";
+  int check = 0;
+  System.out.println("Hello, I am the Riddler.");
+  System.out.println("Please print your name.");
+  String name = scan.nextLine();
+  System.out.println("To capture this flag, you must answer the riddle.");
+  System.out.println(name+", let's start shall we. The start of something new began with the demise of something old.");
+  while(check < 10)
+  {
+  System.out.println("Born the year the Duke defeated Napoleon the bold.");
+  System.out.println("But death came the year the founder of Woolworth Co was born.");
+  System.out.println("I defy the social norms to be rejected and scorned,");
+  System.out.println("For woman I may be yet creator, founder death has made me.");
+  System.out.println("For computers and math are not the sole function");
+  System.out.println("But part of a language, and symbol conjunction.");
+  System.out.println("Who am I?");
+  String answer = scan.nextLine();
+  if(answer.equalsIgnoreCase("Ada Lovelace"))
+  {
+    System.out.println("You have found the flag");
+    System.out.println(flag);
+    check = check + 20;
+  }
+  else
+  {
+    System.out.println("Incorrect");
+  }
+}
+}
+}

afaa769bf23789df87179c3a53e6ba8e368dbce3
Your Name <you@example.com>
2018-06-15 13:59:27 -0500

    Solution to CTF Challenge created

---
+++ CTFChallenge5/program.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+
+public class program {
+  public static void main (String [] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is the encrypting message?");
+    String message = scan.nextLine();
+    String replace1 = message.replace("q","-1-");
+    String replace2 = replace1.replace("w","-2-");
+    String replace3 = replace2.replace("e","-3-");
+    String replace4 = replace3.replace("r","-4-");
+    String replace5 = replace4.replace("t","-5-");
+    String replace6 = replace5.replace("y","-6-");
+    String replace7 = replace6.replace("u","-7-");
+    String replace8 = replace7.replace("i","-8-");
+    String replace9 = replace8.replace("o","-9-");
+    String replace10 = replace9.replace("p","-10-");
+    String replace11 = replace10.replace("a","-11-");
+    String replace12 = replace11.replace("s","-12-");
+    String replace13 = replace12.replace("d","-13-");
+    String replace14 = replace13.replace("f","-14-");
+    String replace15 = replace14.replace("g","-15-");
+    String replace16 = replace15.replace("h","-16-");
+    String replace17 = replace16.replace("j","-17-");
+    String replace18 = replace17.replace("k","-18-");
+    String replace19 = replace18.replace("l","-19-");
+    String replace20 = replace19.replace("z","-20-");
+    String replace21 = replace20.replace("x","-21-");
+    String replace22 = replace21.replace("c","-22-");
+    String replace23 = replace22.replace("v","-23-");
+    String replace24 = replace23.replace("b","-24-");
+    String replace25 = replace24.replace("n","-25-");
+    String replace26 = replace25.replace("m","-26-");
+    String place1 = replace26.replace("Q","-1-");
+    String place2 = place1.replace("W","-2-");
+    String place3 = place2.replace("E","-3-");
+    String place4 = place3.replace("R","-4-");
+    String place5 = place4.replace("T","-5-");
+    String place6 = place5.replace("Y","-6-");
+    String place7 = place6.replace("U","-7-");
+    String place8 = place7.replace("I","-8-");
+    String place9 = place8.replace("O","-9-");
+    String place10 = place9.replace("P","-10-");
+    String place11 = place10.replace("A","-11-");
+    String place12 = place11.replace("S","-12-");
+    String place13 = place12.replace("D","-13-");
+    String place14 = place13.replace("F","-14-");
+    String place15 = place14.replace("G","-15-");
+    String place16 = place15.replace("H","-16-");
+    String place17 = place16.replace("J","-17-");
+    String place18 = place17.replace("K","-18-");
+    String place19 = place18.replace("L","-19-");
+    String place20 = place19.replace("Z","-20-");
+    String place21 = place20.replace("X","-21-");
+    String place22 = place21.replace("C","-22-");
+    String place23 = place22.replace("V","-23-");
+    String place24 = place23.replace("B","-24-");
+    String place25 = place24.replace("N","-25-");
+    String place26 = place25.replace("M","-26-");
+    System.out.println("The encrypted message is below");
+    System.out.println(place26);
+    }
+  }
